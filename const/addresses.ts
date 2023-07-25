d409f24de6227e18954e8466f3bd4b746a9ccad1
Heroes Battle Arena <106152338+heroesbattlearena@users.noreply.github.com>
2023-07-25 10:41:24 +0300

    Update addresses.ts

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,3 +1,3 @@
-export const LOTTERY_CONTRACT_ADDRESS = '0x52fe64465A1456E9D547eC24284Ed135e69e8864';
+export const LOTTERY_CONTRACT_ADDRESS = '0x0eB1019Cb761DEB1eF1095352F1b5bc1485a5d74';
 
 export const HERO_IMAGE_URL = 'https://emerald-far-coral-780.mypinata.cloud/ipfs/QmQNTEFfrbDQgBAMG8nNgj5HMns58apJwAoHZb9eTivyBb';
