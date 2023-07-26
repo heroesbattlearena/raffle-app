f4e51c0924630ab3e19626e466d7b496c333be1d
Heroes Battle Arena <106152338+heroesbattlearena@users.noreply.github.com>
2023-07-26 20:03:39 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import PrizeNFT from "../components/PrizeNFT";
 import { useState } from "react";
 import CurrentEntries from "../components/CurrentEntries";
 import Link from "next/link";
-import Head from "next/head";
 
 
 const Home: NextPage = () => {
@@ -47,25 +46,6 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Head>
-      <title>My page</title>
-      <meta name="facebook-domain-verification" content="7qvotamngngrhn1rkyicuwcbn8r3y6" />
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '835022581502548');
-        fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=835022581502548&ev=PageView&noscript=1"
-        /></noscript>
-    </Head>
     <Container maxW={"1440px"}>
       <SimpleGrid columns={2} spacing={4} minH={"60vh"}>
         <Flex justifyContent={"center"} alignItems={"center"}>
