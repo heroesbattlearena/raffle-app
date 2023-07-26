3f413c7cd95c4f67f12813e8c784d6b9811c013e
Heroes Battle Arena <106152338+heroesbattlearena@users.noreply.github.com>
2023-07-26 22:05:34 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         {/* Add your meta tags, title, etc. within the Head component */}
-        <title>Your Page Title</title>
+        <title>Heroes Battle Arena Raffle | Buy and win</title>
         <meta name="description" content="Your meta description here" />
+        <meta name="facebook-domain-verification" content="7qvotamngngrhn1rkyicuwcbn8r3y6" />
+        <!-- Meta Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '835022581502548');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=835022581502548&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
         {/* Add any other meta tags, CSS, or scripts you need in the <head></head> section */}
       </Head>
       <ThirdwebProvider activeChain={activeChain}>
