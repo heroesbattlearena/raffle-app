00ea3c8a6635af5e8248bb260f1ef33779e14c04
Heroes Battle Arena <106152338+heroesbattlearena@users.noreply.github.com>
2023-07-26 20:21:39 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
-import { Helmet } from 'react-helmet'
+import { Helmet } from "react-helmet";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
